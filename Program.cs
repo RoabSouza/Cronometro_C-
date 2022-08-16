129ef36e2e47c5d3f982cf0419516f82f8348444
Roab Souza <roabsouza144@gmail.com>
2022-08-15 21:25:49 -0300

    Meu segundo app em C# Cronometro

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            menu();
+        }
+
+        static void menu()
+        {
+            Console.Clear();
+            Console.WriteLine("s = Segundos => 10s = 10 segundos");
+            Console.WriteLine("m = Minutos => 1m = 1 Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar ?");
+
+            string data = Console.ReadLine().ToLower();
+            Char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            Console.WriteLine(type);
+            Console.WriteLine(time);
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            prestart(time * multiplier);
+
+
+
+        }
+
+        static void prestart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(1000);
+            Thread.Sleep(2500);
+
+            start(time);
+
+        }
+        static void start(int time)
+        {
+
+            int currentime = 0;
+
+            while (currentime != time)
+            {
+                Console.Clear();
+                currentime++;
+                Console.WriteLine(currentime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("StopWatch finalizado");
+            Thread.Sleep(2500);
+            menu();
+        }
+    }
+}
